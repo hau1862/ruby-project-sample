77ad7cb05fda6173e1e13d5fbdf0ec7430316e0a
hau <hau1862@gmail.com>
2021-07-21 13:10:43 +0700

    Chapter 9: Login Advance

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -4,8 +4,19 @@ module SessionsHelper
   end
 
   def current_user
-    user_id = session[:user_id]
-    @current_user ||= User.find_by(id: user_id) if user_id
+    session_user_id = session[:user_id]
+    cookies_user_id = cookies.signed[:user_id]
+
+    if session_user_id
+      @current_user ||= User.find_by id: session_user_id
+    elsif cookies_user_id
+      user ||= User.find_by id: cookies_user_id
+
+      if user&.authenticated? cookies[:remember_token]
+        log_in user
+        @current_user = user
+      end
+    end
   end
 
   def logged_in?
@@ -13,7 +24,20 @@ module SessionsHelper
   end
 
   def log_out
+    forget current_user
     session.delete :user_id
     @current_user = nil
+  end
+
+  def remember user
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
+  def forget user
+    user.forget
+    cookies.delete :user_id
+    cookies.delete :remember_token
   end
 end
